2383f552f7323dfb2f8574ef1ef04810aec683f5
Snehith <ssssnehith07@gmail.com>
2024-12-24 18:13:48 +0530

    Third Commit

--- Main.java
+++ Main.java
@@ -8,6 +8,7 @@ public static void main(String[] args) {
     System.out.println("This Is Java Program");
     System.out.println(new Date());
     System.out.println("Testing Jenkins after commit");
+    System.out.println("DEMO");
 
 }
 }
