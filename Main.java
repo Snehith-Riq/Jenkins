5fce5e068b35089ca57351b18eb8e2bc58aae75a
Snehith <ssssnehith07@gmail.com>
2024-12-24 14:52:27 +0530

    Second Commit

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.time.Year;
 import java.util.Date;
 
 public class Main{
@@ -6,7 +7,7 @@ public static void main(String[] args) {
    
     System.out.println("This Is Java Program");
     System.out.println(new Date());
-
+    System.out.println("Testing Jenkins after commit");
 
 }
 }
