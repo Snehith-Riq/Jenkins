85df053e51fbfdde1cfb6a30daaf46c2a0f7a55c
Snehith <ssssnehith07@gmail.com>
2024-12-24 13:51:15 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main{
+
+public static void main(String[] args) {
+   
+    System.out.println("This Is Java Program");
+    System.out.println(new Date());
+
+
+}
+}
